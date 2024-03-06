8810d2ab1eb96172e1e47bc050e4c4a78f5f1160
isaac mugwimi <mugwimiisaac230@gmail.com>
2024-03-06 09:38:32 -0500

    new components

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "../Header/Header";
 import "./Hero.css";
+import heroImage from "../assets/hero_image.png";
+import heroImageBack from "../assets/hero_image_back.png";
+import heart from "../assets/heart.png";
+import calories from "../assets/calories.png";
 const Hero = () => {
   return (
     <div className="hero">
@@ -10,12 +14,67 @@ const Hero = () => {
           <div></div>
           <span>THE BEST FITNESS CLUB IN THE TOWN</span>
         </div>
+        {/* hero heading */}
+        <div className="hero-text">
+          <div>
+            <span className="transparent-text">Shape</span>
+            <span> Your</span>
+          </div>
+
+          <div>
+            <span> Ideal Body</span>
+          </div>
+
+          <div className="span">
+            In here we will help you to shape and build your ideal body and live
+            up ou life to the fullest
+          </div>
+        </div>
+
+        {/* Figures */}
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>Expert Coaches</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>Members Joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>Fitness Programs</span>
+          </div>
+        </div>
+
+        {/* Buttons */}
+        <div className="btns">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
       </div>
-      {/* hero heading */}
-      <div className="hero-text">
-        <div><span>Shape</span><span> Your</span></div>
+
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+        <div className="heartRate">
+          <img src={heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+
+        {/* hero images */}
+        <img src={heroImage} alt="" className="heroImage" />
+        <img src={heroImageBack} alt="" className="heroImageback" />
+
+        {/* Calories */}
+        <div className="calories">
+          <img src={calories} alt="" />
+          <div>
+            <span>Calories burned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
       </div>
-      <div className="right-h">right</div>
     </div>
   );
 };
